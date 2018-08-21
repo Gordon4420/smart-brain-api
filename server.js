5294848905364ba30fb3306f7f4a0f57f7ff07a6
Gordon4420 <gordon@gordonmclean.me>
2018-08-22 01:33:04 +0200

    updating database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-globular-38004',
-    user : '',
-    password : '',
-    database : 'Smart-Brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
